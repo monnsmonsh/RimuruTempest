fccdecb5164ade5ce3cce32bc601bfdcb2bc9900
Martín Rodríguez <106488691+monnsmonsh@users.noreply.github.com>
2023-10-06 23:40:45 -0600

    player dx dy
    se add el player y movimiento dx y dy y salto

--- pyGame/Platformer.py
+++ pyGame/Platformer.py
@@ -4,52 +4,137 @@ from pygame.locals import *
 
 pygame.init()
 
-screen_width = 1920	
-screen_heigth = 1080	
+screen_width = 1920
+screen_height = 1080
 
-screen =pygame.display.set_mode((screen_width, screen_heigth))
+screen =pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
 #def variable de juego
-tile_size =200
-
+tile_size = 80
 #cargamos img
-sun_img = pygame.image.load('img/sun.png')
+#sun_img = pygame.image.load('img/sun.png')
 bg_img = pygame.image.load('img/sky.png')
 
 
+#def las guias del videojuago
+ 
+#def el player
+class Player():
+	def __init__(self, x, y):
+		img = pygame.image.load('img/rimuru01.png')
+		self.image = pygame.transform.scale(img, (60,48))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.vel_y = 0
+		self.jumped = False
 
-def draw_grid():
-	for line in range(0, 6):
-		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
-		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+	def update(self):
+		dx= 0
+		dy= 0
+
+		#get keypress
+		key =pygame.key.get_pressed()
+		if key[pygame.K_SPACE] and self.jumped == False:
+			self.vel_y = -10
+			self.jumped = True
+		if key[pygame.K_SPACE] == False:
+			self.jumped = False
+		if key[pygame.K_LEFT]:
+			self.rect.x -= 1
+		if key[pygame.K_RIGHT]:
+			self.rect.x += 1 	
+
+		#agregamos lagravedad
+		self.vel_y += 1
+		if self.vel_y > 10:
+			self.vel_y = 10
+		dy += self.vel_y
+
+		#verificar colicion
+
+		#actualizar cordenadas
+		self.rect.x += dx
+		self.rect.y += dy
+
+		if self.rect.bottom > screen_height:
+			self.rect.bottom = screen_height
+			dy = 0
+
+
+		#player en la plantalla
+		screen.blit(self.image,self.rect)
 
 class World():
-	def __init__(self,data):
+	def __init__(self, data):
+		self.tile_list = []
 		#cargamos img
 		dirt_img = pygame.image.load('img/dirt.png')
-		#grass_img = pygame.image.load('img/grass.png')
+		grass_img = pygame.image.load('img/grass.png')
 
+		row_count = 0
 		for row in data:
+			col_count = 0
 			for tile in row:
+				#dirt
 				if tile == 1:
-					img = pygame.transform.scale(dirt_img,)
+					img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * tile_size
+					img_rect.y = row_count * tile_size
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+				#grass
+				if tile == 2:
+					img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * tile_size
+					img_rect.y = row_count * tile_size
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+				col_count += 1
+			row_count += 1
 
+	def draw(self):
+		for tile in self.tile_list:
+			screen.blit(tile[0], tile[1])
 
-#world
+#world Dibujamos el mundo por bloque activo(1) inactivo(0)
 world_data=[
-[1, 1, 1, 1, 1],
-[1, 0, 0, 0, 1],
-[1, 0, 0, 0, 1],
-[1, 0, 0, 0, 1],
-[1, 1, 1, 1, 1],
+#1--2--3--4--5--6--7--8--9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 0, 0, 0, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[1, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
+
+
+player = Player( 80, screen_height - 170)
+#creamos lineas guia "world = World(world_data)"
+world = World(world_data)
 
 run = True
 while run:
 	
 	screen.blit(bg_img, (0, 0))
-	screen.blit(sun_img,(100,100))
+	#screen.blit(sun_img,(100,100))
+
+	world.draw()
+
+	player.update()
+
+	
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
